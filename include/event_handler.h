234cb46395dbf05cba2d0cca7c77fc4b893d96d8
jordan <lijun.xue.jordan@gmail.com>
2015-03-15 22:48:05 +0800

    synchronize.

--- include/event_handler.h
+++ include/event_handler.h
@@ -1,10 +1,14 @@
 #ifndef EVENT_HANDLER_H_
 #define EVENT_HANDLER_H_
 
+namespace magic_bean {
+
 template<typename T>
 class EventHandler {
  public:
-  virtual void OnEvent(const T& event, uint64_t sequence, bool end_of_batch) = 0;
+  virtual void OnEvent(const T& event, int64_t sequence, bool end_of_batch) = 0;
 };
+
+} //end namespace
 
 #endif //EVENT_HANDLER_H_
--- include/sequence.h
+++ include/sequence.h
@@ -1,23 +1,38 @@
 #ifndef SEQUENCE_H_
 #define SEQUENCE_H_
 
+#include <atomic>
+#include <cstdint>
+
+namespace magic_bean{
+
 class LhsPadding {
- protected:
+ public:
   int64_t p1_, p2_, p3_, p4_, p5_, p6_, p7_;
 };
 
 class Value : public LhsPadding {
- protected:
-  volatile int64_t value_;
+ public:
+  std::atomic<int64_t> value_;
 };
 
 class RhsPadding : public Value {
- protected:
+ public:
   int64_t p9_, p10_, p11_, p12_, p13_, p14_, p15_;
 };
 
 class Sequence : public RhsPadding {
  public:
+  Sequence();
+  Sequence(int64_t initial_value);
+
+  int64_t Get() const;
+  void Set(int64_t value);
+
+  int64_t IncrementAndGet();
+  int64_t AddAndGet(int64_t increment);
 };
+
+} //end namespace
 
 #endif //SEQUENCE_H_
