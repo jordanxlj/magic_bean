e5b3db9fc8bbba022c91b22e35042766498c1be7
jordan <lijun.xue.jordan@gmail.com>
2015-03-19 00:12:05 +0800

    add sequence barrier.

---
+++ src/sequence_barrier.h
@@ -0,0 +1,21 @@
+#ifndef SEQUENCE_BARRIER_H_
+#define SEQUENCE_BARRIER_H_
+
+namespace magic_bean {
+
+class SequenceBarrier {
+ public:
+  virtual int64_t WaitFor(int64_t sequence) throw (AlertException, TimeoutException) = 0;
+
+  virtual int64_t GetCursor() const = 0;
+
+  virtual bool IsAlerted() const = 0;
+  virtual void Alert() = 0;
+
+  virtual void ClearAlert() = 0;
+  virtual CheckAlert() throw AlertException = 0;
+};
+
+} //end namespace
+
+#endif //SEQUENCE_BARRIER_H_
