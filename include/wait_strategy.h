318e9bdbeeb61822a3c659a162b54633cbf018b6
jordan <lijun.xue.jordan@gmail.com>
2015-03-20 00:11:06 +0800

    modify sequence barrier.

--- include/wait_strategy.h
+++ include/wait_strategy.h
@@ -1,12 +1,21 @@
 #ifndef WAIT_STRATEGY_H_
 #define WAIT_STRATEGY_H_
 
+#include <cstdint>
+
 namespace magic_bean {
+
+class Sequence;
+class SequenceBarrier;
 
 class WaitStrategy{
  public:
   virtual ~WaitStrategy() {};
 
+  virtual int64_t WaitFor(int64_t sequence, Sequence& cursor, Sequence dependent_sequence, SequenceBarrier& barrier) = 0;
+  virtual void SignalAllWhenBlocking() = 0;
 };
+
+} //end namespace
 
 #endif //WAIT_STRATEGY_H_
