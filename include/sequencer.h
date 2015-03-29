c0b77571569be7126c456f3cc5b30add739a3c59
jordan <lijun.xue.jordan@gmail.com>
2015-03-29 11:54:38 +0800

    add sequencer.h

---
+++ include/sequencer.h
@@ -0,0 +1,28 @@
+#ifndef SEQUENCER_H_
+#define SEQUENCER_H_
+
+#include <vector>
+#include "cursored.h"
+#include "magic_types.h"
+#include "sequence.h"
+#include "sequenced.h"
+
+namespace magic_bean {
+
+class Sequencer : public Cursored, public Sequenced {
+ public:
+  virtual ~Sequencer() {};
+
+  virtual void Claim(int64_t sequence) = 0;
+  virtual bool IsAvailable(int64_t sequence) = 0;
+
+  virtual void AddGatingSequences(const std::vector<SequencePtr>& gating_sequences) = 0;
+  virtual bool RemoveGatingSequence(SequencePtr gating_sequence) = 0;
+
+  virtual int64_t GetMinimumSequence() const = 0;
+  virtual int64_t GetHighestPublishedSequence(int64_t next_sequence, int64_t available_sequence) = 0;
+};
+
+} //end namespace
+
+#endif //SEQUENCER_H_
