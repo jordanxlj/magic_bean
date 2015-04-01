33d0a2613b7b8e2c0db07c7f271719bd04d30cab
jordan <lijun.xue.jordan@gmail.com>
2015-04-02 00:00:12 +0800

    modify wait strategy interface.

--- include/wait_strategy.h
+++ include/wait_strategy.h
@@ -1,18 +1,20 @@
 #ifndef WAIT_STRATEGY_H_
 #define WAIT_STRATEGY_H_
 
-#include <cstdint>
+#include "alert_exception.h"
+#include "magic_types.h"
+#include "timeout_exception.h"
 
 namespace magic_bean {
 
-class Sequence;
 class SequenceBarrier;
 
 class WaitStrategy{
  public:
   virtual ~WaitStrategy() {};
 
-  virtual int64_t WaitFor(int64_t sequence, Sequence& cursor, Sequence dependent_sequence, SequenceBarrier& barrier) = 0;
+  virtual int64_t WaitFor(int64_t sequence, SequencePtr cursor, Sequence dependent_sequence,
+                          SequenceBarrier* barrier) throw (AlertException, TimeoutException) = 0;
   virtual void SignalAllWhenBlocking() = 0;
 };
 
