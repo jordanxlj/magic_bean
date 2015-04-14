4f69f9cfecaf413d32d7be37f9d4491170cfac2c
jordan <lijun.xue.jordan@gmail.com>
2015-04-14 23:44:33 +0800

    add test sequencer case.

--- test/test_sequencer.cc
+++ test/test_sequencer.cc
@@ -2,6 +2,9 @@
 #include "multi_producer_sequencer.h"
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <functional>
+#include <thread>
+#include <condition_variable>
 #include "blocking_wait_strategy.h"
 #include "sequence.h"
 
@@ -13,6 +16,13 @@ enum ProducerType {
 };
 
 class SequencerTest : public ::testing::TestWithParam<int> {
+ public:
+  void Execute() {
+    waiting_cond.notify_all();
+    int64_t next = sequencer->Next();
+    sequencer->Publish(next);
+    done_cond.notify_all();
+  }
  protected:
   SequencerTest() {
     producer_type = GetParam();
@@ -44,6 +54,11 @@ class SequencerTest : public ::testing::TestWithParam<int> {
   WaitStrategy* wait_strategy;
   SequencePtr gating_sequence;
   static const int BUFFER_SIZE = 16;
+  static const int64_t INITIAL_CURSOR_VALUE = -1;
+  std::condition_variable waiting_cond;
+  std::mutex waiting_mutex;
+  std::condition_variable done_cond;
+  std::mutex done_mutex;
 };
 
 TEST_P(SequencerTest, should_start_with_initial_value) {
@@ -66,6 +81,40 @@ TEST_P(SequencerTest, should_indicate_has_available_capacity) {
   sequencer->Publish(sequencer->Next());
   ASSERT_TRUE(sequencer->HasAvailableCapacity(BUFFER_SIZE - 1));
   ASSERT_FALSE(sequencer->HasAvailableCapacity(BUFFER_SIZE));
+}
+
+TEST_P(SequencerTest, should_indicate_no_available_capacity) {
+  std::vector<SequencePtr> gating_sequences;
+  gating_sequences.push_back(gating_sequence);
+  sequencer->AddGatingSequences(gating_sequences);
+
+  int64_t sequence = sequencer->Next(16);
+  sequencer->Publish(sequence - (BUFFER_SIZE - 1), sequence);
+  ASSERT_FALSE(sequencer->HasAvailableCapacity(1));
+}
+
+TEST_P(SequencerTest, should_holdup_publisher_when_buffer_is_full) {
+  std::vector<SequencePtr> gating_sequences;
+  gating_sequences.push_back(gating_sequence);
+  sequencer->AddGatingSequences(gating_sequences);
+
+  int64_t sequence = sequencer->Next(BUFFER_SIZE);
+  sequencer->Publish(sequence - (BUFFER_SIZE - 1), sequence);
+
+  std::unique_lock<std::mutex> done_lock(done_mutex);
+  std::unique_lock<std::mutex> waiting_lock(waiting_mutex);
+
+  int64_t expected_full_sequence = INITIAL_CURSOR_VALUE + sequencer->GetBufferSize();
+  ASSERT_EQ(sequencer->GetCursor(), expected_full_sequence);
+
+  std::thread thread(std::bind(&SequencerTest::Execute, this));
+
+  waiting_cond.wait(waiting_lock);
+  ASSERT_EQ(sequencer->GetCursor(), expected_full_sequence);
+  gating_sequence->Set(INITIAL_CURSOR_VALUE + 1);
+  done_cond.wait(done_lock);
+
+  ASSERT_EQ(sequencer->GetCursor(), expected_full_sequence + 1);
 }
 
 INSTANTIATE_TEST_CASE_P(AllProducerSequencerTest, SequencerTest, ::testing::Range(0, 2));
