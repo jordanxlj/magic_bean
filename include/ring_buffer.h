c5df7af68e2377416c2a3c9a862e4f720f25ee71
jordan <lijun.xue.jordan@gmail.com>
2015-04-18 20:28:35 +0800

    refactoring ringbuffer method parameter.

--- include/ring_buffer.h
+++ include/ring_buffer.h
@@ -89,13 +89,13 @@ class RingBuffer : public RingBufferFields<T>, public Cursored, public DataProvi
  public:
   RingBuffer(EventFactory<T>* event_factory, Sequencer* sequencer);
   ~RingBuffer();
-  static RingBuffer<T>* CreateMultiProducer(EventFactory<T>* factory,
+  static RingBuffer<T>* CreateMultiProducer(EventFactory<T>* event_factory,
                                             int buffer_size, WaitStrategy* wait_strategy);
-  static RingBuffer<T>* CreateMultiProducer(EventFactory<T>* factory, int buffer_size);
+  static RingBuffer<T>* CreateMultiProducer(EventFactory<T>* event_factory, int buffer_size);
 
-  static RingBuffer<T>* CreateSingleProducer(EventFactory<T>* factory,
+  static RingBuffer<T>* CreateSingleProducer(EventFactory<T>* event_factory,
                                              int buffer_size, WaitStrategy* wait_strategy);
-  static RingBuffer<T>* CreateSingleProducer(EventFactory<T>* factory, int buffer_size);
+  static RingBuffer<T>* CreateSingleProducer(EventFactory<T>* event_factory, int buffer_size);
 
   virtual T* Get(int64_t sequence) override;
   virtual int64_t Next() override;
@@ -135,31 +135,31 @@ template<typename T>
   RingBuffer<T>::~RingBuffer() {}
 
 template<typename T>
-  RingBuffer<T>* RingBuffer<T>::CreateMultiProducer(EventFactory<T>* factory,
+  RingBuffer<T>* RingBuffer<T>::CreateMultiProducer(EventFactory<T>* event_factory,
                                                     int buffer_size,
                                                     WaitStrategy* wait_strategy) {
   Sequencer* sequencer = new MultiProducerSequencer(buffer_size, wait_strategy);
-  return new RingBuffer<T>(factory, sequencer);
+  return new RingBuffer<T>(event_factory, sequencer);
 }
 
 template<typename T>
-  RingBuffer<T>* RingBuffer<T>::CreateMultiProducer(EventFactory<T>* factory,
+  RingBuffer<T>* RingBuffer<T>::CreateMultiProducer(EventFactory<T>* event_factory,
                                                     int buffer_size) {
-  return CreateMultiProducer(factory, buffer_size, new BlockingWaitStrategy());
+  return CreateMultiProducer(event_factory, buffer_size, new BlockingWaitStrategy());
 }
 
 template<typename T>
-  RingBuffer<T>* RingBuffer<T>::CreateSingleProducer(EventFactory<T>* factory,
+  RingBuffer<T>* RingBuffer<T>::CreateSingleProducer(EventFactory<T>* event_factory,
                                                      int buffer_size,
                                                      WaitStrategy* wait_strategy) {
   Sequencer* sequencer = new SingleProducerSequencer(buffer_size, wait_strategy);
-  return new RingBuffer<T>(factory, sequencer);
+  return new RingBuffer<T>(event_factory, sequencer);
 }
 
 template<typename T>
-  RingBuffer<T>* RingBuffer<T>::CreateSingleProducer(EventFactory<T>* factory,
+  RingBuffer<T>* RingBuffer<T>::CreateSingleProducer(EventFactory<T>* event_factory,
                                                      int buffer_size) {
-  return CreateSingleProducer(factory, buffer_size, new BlockingWaitStrategy());
+  return CreateSingleProducer(event_factory, buffer_size, new BlockingWaitStrategy());
 }
 
 template<typename T>
