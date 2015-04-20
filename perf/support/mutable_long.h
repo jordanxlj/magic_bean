4b0c871aa819703a12e56d78b5b3e95773ba5be1
jordan <lijun.xue.jordan@gmail.com>
2015-04-20 23:51:48 +0800

    add mutable long.

---
+++ perf/support/mutable_long.h
@@ -0,0 +1,31 @@
+#ifndef MUTABLE_LONG_H_
+#define MUTABLE_LONG_H_
+
+#include <cstdint>
+
+class MutableLong {
+ public:
+  MutableLong()
+    : value_(0) {}
+
+  MutableLong(int64_t initial_value)
+    : value_(initial_value) {}
+
+  int64_t Get() {
+    return value_;
+  }
+
+  void Set(int64_t value) {
+    value_ = value;
+  }
+
+  void Increment() {
+    value_++;
+  }
+ private:
+  int64_t value_;
+};
+
+} //end namespace
+
+#endif //UTIL_H_
